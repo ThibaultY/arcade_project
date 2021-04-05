28c464ca93a93a961e92860604b4beb68f813c21
ThibaultY <78028931+ThibaultY@users.noreply.github.com>
2021-04-05 15:46:15 -0400

    dvd is now complet

--- dvd_waiting_screen.py
+++ dvd_waiting_screen.py
@@ -6,7 +6,7 @@ télévision.
 import arcade
 from dataclasses import dataclass
 
-SCREEN_WIDTH = 800
+SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 COLOR_LIST = [
     arcade.color.BLUE,
@@ -26,15 +26,51 @@ class Cercle:
     centre_y: int
     rayon: int
     couleur: (int, int, int)
-    x_update = 3
-    y_update = 3
+    x_mouvement = 300
+    y_mouvement = 300
 
     def draw(self):
         arcade.draw_circle_filled(self.centre_x, self.centre_y, self.rayon, self.couleur)
 
-    def update(self):
+    def update(self, delta_time):
+        # changing position of the circle
+        self.centre_x += self.x_mouvement * delta_time
+        self.centre_y += self.y_mouvement * delta_time
 
-        if
+        # checking and correcting colitions with the wall
+        if self.centre_x + self.rayon >= SCREEN_WIDTH or self.centre_x - self.rayon <= 0:
+            self.x_mouvement *= -1  # changes the direction
+            self.centre_x += self.x_mouvement * delta_time  # keeps the circle form entering the wall before next update
+
+        if self.centre_y + self.rayon >= SCREEN_HEIGHT or self.centre_y - self.rayon <= 0:
+            self.y_mouvement *= -1
+            self.centre_y += self.y_mouvement * delta_time
 
 
+class MyGame(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, 'DvD', update_rate=1/120)
+        self.liste_cercle = []
 
+    def setup(self):
+        self.liste_cercle.append(Cercle(300, 300, 50, arcade.color.BRIGHT_GREEN))
+        arcade.set_background_color(arcade.color.BLACK)
+
+    def on_draw(self):
+        arcade.start_render()
+        for cercle in self.liste_cercle:
+            cercle.draw()
+
+    def on_update(self, delta_time: float):
+        for cercle in self.liste_cercle:
+            cercle.update(delta_time)
+
+
+def main():
+    my_game = MyGame()
+    my_game.setup()
+    arcade.run()
+
+
+if __name__ == '__main__':
+    main()
